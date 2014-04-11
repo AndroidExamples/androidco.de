780f4bb36992b496cec17b027a1431759b3aca0a
Hans Cappelle <hans.cappelle@gmail.com>
2014-04-11 17:34:23 +0200

    introduced bower complete, added angular, ui routing

---
+++ public/js/app.js
@@ -0,0 +1,17 @@
+var myApp = angular.module('myApp', ['ui.router']);
+
+myApp.config(function($stateProvider, $urlRouterProvider){
+
+    // For any unmatched url, send to /route1
+    $urlRouterProvider.otherwise("/home")
+
+    $stateProvider
+        .state('home', {
+            url: "/home",
+            templateUrl: "partials/home.html"
+        })
+        .state('about', {
+            url: "/about",
+            templateUrl: "partials/about.html"
+        })
+})
