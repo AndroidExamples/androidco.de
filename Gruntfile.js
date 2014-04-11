defae2d6efff0bda20757978482b57063d04b7c1
Hans Cappelle <hans.cappelle@gmail.com>
2014-04-11 20:03:34 +0200

    enabled uglification of scripts and moved scripts to src folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,12 +23,43 @@ module.exports = function(grunt) {
         },
 
         // uglification of scripts
-        // TODO
+        uglify: {
+            dev: {
+                options: {
+                    banner: '/*! <%= pkg.name %> v<%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                    beautify: true,
+                    mangle: false,
+                    compress: false
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src/scripts',
+                    src: '**/*.js',
+                    dest: 'public/js/'
+                }]
+            },
+            staging: {
+                options: {
+                    banner: '/*! <%= pkg.name %> v<%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'src/scripts',
+                    src: '**/*.js',
+                    dest: 'public/js/'
+                }]
+            }
+
+        },
 
         watch: {
             templates:{
                 files: ['src/views/*.jade'],
                 tasks: ['jade']
+            },
+            scripts: {
+                files: ['src/scripts/*.js'],
+                tasks: ['uglify:dev']
             }
         }
     });
@@ -36,8 +67,10 @@ module.exports = function(grunt) {
     // load plugins
     grunt.loadNpmTasks('grunt-contrib-jade');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     //Default task(s).
-    grunt.registerTask('default', ['jade']);
+    grunt.registerTask('default', ['jade', 'uglify:staging']);
+    grunt.registerTask('dev', ['jade', 'uglify:dev']);
 
 };
--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,2 @@
-var myApp = angular.module('myApp', ['ui.router']);
-
-myApp.config(function($stateProvider, $urlRouterProvider){
-
-    // For any unmatched url, send to /route1
-    $urlRouterProvider.otherwise("/home")
-
-    $stateProvider
-
-        .state('home', {
-            url: "/home",
-            templateUrl: "partials/home.html"
-        })
-
-        .state('projects', {
-            url: "/projects",
-            templateUrl: "partials/projects.html"
-        })
-
-        .state('about', {
-            url: "/about",
-            templateUrl: "partials/about.html"
-        })
-})
+/*! androidco-de-website v0.1.0 2014-04-11 */
+var myApp=angular.module("myApp",["ui.router"]);myApp.config(function(a,b){b.otherwise("/home"),a.state("home",{url:"/home",templateUrl:"partials/home.html"}).state("projects",{url:"/projects",templateUrl:"partials/projects.html"}).state("about",{url:"/about",templateUrl:"partials/about.html"})});
