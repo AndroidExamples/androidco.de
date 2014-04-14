196ef24be91300bdc7436fa58ce868f73582db29
Hans Cappelle <hans.cappelle@gmail.com>
2014-04-14 11:45:33 +0200

    progress impl md rendering

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,18 @@
 /*! androidco-de-website v0.1.0 2014-04-14 */
-"use strict";var myApp=angular.module("myApp",["ui.router","yaru22.md"]);myApp.config(function(a,b){b.otherwise("/home"),a.state("home",{url:"/home",templateUrl:"partials/home.html"}).state("projects",{url:"/projects",templateUrl:"partials/projects.html"}).state("about",{url:"/about",templateUrl:"partials/about.html"})}).controller("mdExampleCtrl",function(a){a.mdText="## Example 3\nThis is the third example. Modify the text in the text box and see it being rendered on the fly!"});
+"use strict";
+
+angular.module("myApp", [ "ui.router", "yaru22.md" ]).config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/home");
+    $stateProvider.state("home", {
+        url: "/home",
+        templateUrl: "partials/home.html"
+    }).state("projects", {
+        url: "/projects",
+        templateUrl: "partials/projects.html"
+    }).state("about", {
+        url: "/about",
+        templateUrl: "partials/about.html"
+    });
+}).controller("mdExampleCtrl", function($scope) {
+    $scope.mdText = "## Example 3\nThis is the third example. Modify the text in the text box and see it being rendered on the fly!";
+});
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,8 @@
 'use strict'
 
-var myApp = angular.module('myApp', ['ui.router', 'yaru22.md']);
+angular.module('myApp', ['ui.router', 'yaru22.md'])
 
-myApp.config(function($stateProvider, $urlRouterProvider){
+.config(function($stateProvider, $urlRouterProvider){
 
     // For any unmatched url, send to /route1
     $urlRouterProvider.otherwise("/home");
@@ -27,4 +27,4 @@ myApp.config(function($stateProvider, $urlRouterProvider){
 
 .controller('mdExampleCtrl', function($scope){
     $scope.mdText = '## Example 3\nThis is the third example. Modify the text in the text box and see it being rendered on the fly!';
-  });
+});
