8b7a052cdcaab985bd7f606b5bbbc993c2bb723f
Hans Cappelle <hans.cappelle@gmail.com>
2014-04-15 09:23:54 +0200

    update marked content now coming from projects.md file

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,43 +2,44 @@
 
 angular.module('myApp', ['ui.router', 'yaru22.md'])
 
-.config(function($stateProvider, $urlRouterProvider){
+  .config(function ($stateProvider, $urlRouterProvider) {
 
     // For any unmatched url, send to /route1
     $urlRouterProvider.otherwise("/home");
 
     $stateProvider
 
-        .state('home', {
-            url: "/home",
-            templateUrl: "partials/home.html"
-        })
+      .state('home', {
+        url: "/home",
+        templateUrl: "partials/home.html"
+      })
 
-        .state('projects', {
-            url: "/projects",
-            templateUrl: "partials/projects.html"
-        })
+      .state('projects', {
+        url: "/projects",
+        templateUrl: "partials/projects.html"
+      })
 
-        .state('about', {
-            url: "/about",
-            templateUrl: "partials/about.html"
-        });
-})
+      .state('about', {
+        url: "/about",
+        templateUrl: "partials/about.html"
+      });
+  })
 
-  .controller('ProjectsCtrl', function($scope, $http){
+  .controller('ProjectsCtrl', function ($scope, $http) {
 
     // retrieve raw project md files here
-    $http({method: 'GET', url: 'http://github-raw-cors-proxy.herokuapp.com/hanscappelle/more-android-examples/master/README.md'})
-      .success(function(data) {
+    // TODO move this to config instead
+    $http({method: 'GET', url: 'https://raw.githubusercontent.com/AndroidExamples/androidco.de/master/PROJETS.md'})
+      .success(function (data) {
         // this callback will be called asynchronously
         // when the response is available
-        $scope.readme = data.substring(1, data.length-1).replace(/\\n/g, '\r\n');
+        $scope.markedContent = data.substring(1, data.length - 1).replace(/\\n/g, '\r\n');
       })
-      .error(function() {
+      .error(function () {
         // called asynchronously if an error occurs
         // or server returns response with an error status.
-        console.log('problem retrieving readme file');
-        $scope.readme = 'problem retrieving readme file';
+        console.log('problem retrieving marked content file');
+        $scope.markedContent = 'problem retrieving marked content file';
       });
 
   });
